1e7ea24d568c0ab78cec405bae86c3c82bdf48f7
patrik <patrik>
2001-12-05 07:01:08 +0000

    Added the GPL text.

--- src/hook.h
+++ src/hook.h
@@ -1,3 +1,26 @@
+/*
+  VR Mode - integration of GNU Emacs and Dragon NaturallySpeaking.
+
+  Copyright 1999 Barry Jaspan, <bjaspan@mit.edu>.  All rights reserved.
+
+  This file is part of Emacs VR Mode.
+
+  Emacs VR Mode is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at
+  your option) any later version.
+
+  Emacs VR Mode is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Emacs VR Mode; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA
+*/
+
 #ifndef __VR_HOOK_H__
 #define __VR_HOOK_H__
 
